5caa99daf66eff398f127c85cc791a50ae1814ca
1951397915 <60497349+1951397915@users.noreply.github.com>
2023-03-05 23:30:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -36,7 +36,7 @@ const USER_CONFIG = {
 //           type: '生日', name: '李四', year: '1996', date: '09-31',
 //         },
         {
-          type: '节日', name: '我们认识', year: '2020', date: '09-15',
+          type: '节日', name: '我们相识纪念日', year: '2020', date: '09-15',
         },
         {
           type: '节日', name: '考试', year: '2023', date: '04-22',
